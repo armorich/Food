2555eab382c21ef93963d22b565b67a56ed9c009
Артём М <ahtyoma@gmail.com>
2022-07-20 20:03:09 +0300

    Added Thanks Modal Window

--- js/script.js
+++ js/script.js
@@ -101,8 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Modal 
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
     function openModal() {
         // modal.classList.add('show');
@@ -125,22 +124,21 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = "";
     }
 
-    modalCloseBtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (event) => {
-        if (event.target === modal) {
+        if (event.target === modal || event.target.getAttribute('data-close') == '') {
             closeModal();
         }
     });
 
-    document, addEventListener('keydown', (e) => {
+    document.addEventListener('keydown', (e) => {
         if (e.code === "Escape" && modal.classList.contains('show')) {
             closeModal();
         }
     });
 
 
-    const modalTimerId = setTimeout(openModal, 3000); //Модальное окно, которое появляется каждые 3 секунды 
+    const modalTimerId = setTimeout(openModal, 50000); //Модальное окно, которое появляется каждые 3 секунды 
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -222,18 +220,23 @@ document.addEventListener('DOMContentLoaded', () => {
         '.menu .container'
     ).render();
 
-    // Forms (AJAX запросы с использованием локального сервера MAMP)
 
 
-    // Объект в котором находятся статусы запросов, пригодиться для оповещения пользователя о статусе
-    const message = {
-        loading: 'Загрузка',
-        success: 'Спсибо! Мы с вами скоро свяжемся',
-        failure: 'Что-то пошло не так...'
-    };
+
+    //? Forms (AJAX запросы с использованием локального сервера MAMP)
+
+
+
 
     // Переменная которая по тегу <form> получает элементы
     const forms = document.querySelectorAll('form');
+
+    // Объект в котором находятся статусы запросов, пригодиться для оповещения пользователя о статусе
+    const message = {
+        loading: 'img/form/005 spinner.svg',
+        success: 'Спсибо! Мы с вами скоро свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
 
     forms.forEach(item => { // Перебор всех тегов <form>
         postData(item); // Исппользование ф-ии для одной из пребранных форм
@@ -245,15 +248,19 @@ document.addEventListener('DOMContentLoaded', () => {
             e.preventDefault(); //Сбро стандартного поведения браузера
 
             // Создание элемента на странице, в котором будет написан статус отправки данных на сервер
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status'); // Присваивание уведомлению класса .status
-            statusMessage.textContent = message.loading; // Сразу после отправки присвоение статуса загрузки
-            form.append(statusMessage); // Добавление в конец полученной формы статуса
+            let statusMessage = document.createElement('img');
+            statusMessage.src = message.loading; // Присваивание уведомлению класса .status
+            statusMessage.style.cssText = `
+                display: block; 
+                margin: 0 auto;
+            `; // Сразу после отправки присвоение статуса загрузки
+            // form.append(statusMessage); // Добавление в конец полученной формы статуса
+            form.insertAdjacentElement('afterend', statusMessage);
+
 
             const request = new XMLHttpRequest(); // Моздание HTTP объекта, которые далее должен передать запрос на сервер
             request.open('POST', 'server.php'); // Метод делающий пост запрос серверу 
-
-            // request.setRequestHeader('Content-type', 'application/json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
 
             // Для создания объекта и для дальнейшей конвертации его в JSON
@@ -266,28 +273,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Конвертация объекта в JSON
             //! PHP не умеет работать с JSON поэтому надо будет конвертировать в подходящий формат, в дальнейшем
-            // const json = JSON.stringify(object);
-            // request.send(json);
+            const json = JSON.stringify(object); // Превращение из обычного объекта в JSON
+            request.send(json);
 
 
             // const formData = new FormData(form); // Создание пар ключ значение, аргументы для этого получаются с формы
 
-            // Из-за спцифичночти FormData данные надо будет первести в 
+            // Из-за спцифичночти FormData данные надо будет первести в JSON
 
-            request.send(formData);
+            // request.send(formData);
 
             request.addEventListener('load', () => {
                 if (request.status === 200) {
-                    console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    console.log(request.response); // Отображает в консоли переданные данные
+                    showThanksModal(message.success); // Вызывается модальное окно с сообщением о благодарности 
+                    statusMessage.remove(); // Удаление сообщения о статусе
                     form.reset(); // так производится очистка формы
-                    setTimeout(() => { // Интервал, после которого соощение о статусе запроса удаляется (если успешно)
-                        statusMessage.remove();
-                    }, 2000);
                 } else {
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure); // Вызывается модальное окно с сообщением об ошибке
                 }
             });
         });
+    }
+
+    // Modal with thanks message
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog'); // Поулчаем диалоговое окно, сам блок
+
+        prevModalDialog.classList.add('hide'); // Сразу же скрываем его
+        // openModal(); // Ф-я отвеччающая за открытие окна, переключает классы
+
+        const thanksModal = document.createElement('div'); // Создание окна благодарности
+        thanksModal.classList.add('modal__dialog'); // Добавление класса, основы для окна 
+
+        // Добавление на страницу модального окна, в котором описывается и состояние отправки формы
+        thanksModal.innerHTML = `  
+            <div class="modal__content">
+                <div class="modal__close" data-close>×</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+
+        document.querySelector('.modal').append(thanksModal); // Добавленние окна благодарности
+        setTimeout(() => { // Создание асинхронного кода по удалению окна после отправки формы
+            thanksModal.remove(); // Удаление окна благодарности
+            prevModalDialog.classList.add('show'); // Две следующие операции создают и скрывают модальное оконо с формой
+            prevModalDialog.classList.remove('hide');
+            closeModal(); // Ф-я закрытия модального окна 
+        }, 4000);
     }
 });
